b2bb9aa1a3eb8d3d81c912f0df778287e172865c
Saltanat <152485845+SaltanatDan@users.noreply.github.com>
2023-12-10 20:22:22 +0600

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+#Выводит тип переменной
+a=15.5
+print(type(a))
+b=6
+print(type(b))
+c=16.78
+print(type(c))
+
+#изменение типа данных
+a=int(a)
+print("a=", a)
+
+a = 10
+print("Десять " + str(a))
