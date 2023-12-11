5938df301c4b26cbd77e292a93990060910ba5b7
Saltanat <152485845+SaltanatDan@users.noreply.github.com>
2023-12-11 22:00:27 +0600

    datatypes.py

--- datatypes.py
+++ datatypes.py
@@ -12,3 +12,17 @@ print("a=", a)
 
 a = 10
 print("Десять " + str(a))
+
+a = 'я изучаю Python' 
+print(list(a))
+
+a = 'привет'
+print(set(a))
+
+a = ('red', 'blue', 'green')
+print(list(a))
+
+name = input('Как тебя зовут? ')
+print(f'Привет, {name}!')
+age = int(input('Сколько тебе лет? '))
+print(f'Здорово! В следующем году тебе будет {age + 1}!')
